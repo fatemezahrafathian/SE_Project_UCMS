c39c53106246e360fb5a7b6d76efb9eff7c26aa0
Hossein0402 <h.ahmadi0402@gmail.com>
2025-04-18 22:20:06 +0330

    start student task

---
+++ UCMS_Implementation/TestProject1/Controllers/UserControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using UCMS.Services.UserService;
+
+namespace TestProject1.Controllers
+{
+    internal class UserControllerTest
+    {
+        private readonly Mock<IUserService> _mockUserService;
+        private readonly UserController _controller;
+
+        public UserControllerTests()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _controller = new UserController(_mockUserService.Object);
+        }
+    }
+}
---
+++ UCMS_Implementation/UCMS/DTOs/Student/EditStudentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace UCMS.DTOs.Student
+{
+    public class EditStudentDto
+    {
+        public string? Major { get; set; }
+        public int? EnrollmentYear { get; set; }
+        public int? University { get; set; }
+        public int? مقطع { get; set; }
+    }
+}
