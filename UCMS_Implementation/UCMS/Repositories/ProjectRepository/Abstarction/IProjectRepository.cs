c51574fa35396dea6bdea244169b722b5332d9e0
HanaNrvtn <hanaNrvtn@gmail.com>
2025-06-25 05:10:58 -0700

    test(project-repository): all functions tested.

--- UCMS_Implementation/UCMS/Repositories/ProjectRepository/Abstarction/IProjectRepository.cs
+++ UCMS_Implementation/UCMS/Repositories/ProjectRepository/Abstarction/IProjectRepository.cs
@@ -8,17 +8,14 @@ public interface IProjectRepository
     Task AddAsync(Project project);
     Task<Project?> GetProjectByIdAsync(int projectId);
     Task<Project?> GetSimpleProjectByIdAsync(int projectId);
+    Task<Project?> GetProjectWithRelationsByIdAsync(int projectId);
     Task UpdateAsync(Project project);
     Task DeleteAsync(Project project);
 
     Task<List<Project>> FilterProjectsForInstructorAsync(int instructorId, string? title, string? classTitle,
         int? projectStatus, string orderBy, bool descending);
-
     Task<List<Project>> FilterProjectsForStudentAsync(int instructorId, string? title, string? classTitle,
         int? projectStatus, string orderBy, bool descending);
-    Task<bool> IsProjectForInstructorAsync(int projectId, int instructorId);
-    Task<bool> IsProjectForStudentAsync(int projectId, int studentId);
-    Task<bool> ProjectExists(int projectId);
     Task<bool> IsProjectNameDuplicateAsync(int classId, string projectName);
     Task<List<Project>> GetProjectsByClassIdAsync(int classId);
 
